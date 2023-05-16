87b368a3d9fb23740317305b00df9fcf8e2cd012
Werton <mr.werton@gmail.com>
2023-05-16 00:52:52 -0300

    created home page

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const drawer = document.querySelector("#drawer")
+const btn_drawer = document.querySelector(".btn_drawer")
+const btn_drawer_close = document.querySelector(".btn_drawer_close")
+
+btn_drawer.addEventListener('click', handleOpenDrawer)
+btn_drawer_close.addEventListener('click', handleOpenDrawer)
+
+function handleOpenDrawer(){
+    console.log("here")
+    drawer.classList.toggle("visible")
+}
